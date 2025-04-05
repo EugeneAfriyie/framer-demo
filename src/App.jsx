f412ff05f0bd687a8900f35b9984b36dfa1f30f9
Eugene Afriyie <groupeight00@gmail.com>
2025-04-05 02:01:04 +0000

    created imagegallery tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AnimatedCard from './Component/AnimatedCard'
+import ImageGallery from './Component/ImageGallery'
 
 const App = () => {
   return (
     <div>
-      <AnimatedCard />
+      {/* <AnimatedCard /> */}
+      <ImageGallery />
     </div>
   )
 }
