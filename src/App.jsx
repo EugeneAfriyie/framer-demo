02a200db95d5013faf65ba9964cd630077077cd3
Eugene Afriyie <groupeight00@gmail.com>
2025-04-06 23:49:09 +0000

    Animated Gallery jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AnimatedCard from './Component/AnimatedCard'
 import ImageGallery from './Component/ImageGallery'
+import AnimaterdGallery from './Component/AnimaterdGallery'
 
 const App = () => {
   return (
     <div>
       {/* <AnimatedCard /> */}
-      <ImageGallery />
+      {/* <ImageGallery /> */}
+      <AnimaterdGallery />
     </div>
   )
 }
---
+++ src/Component/AnimaterdGallery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnimaterdGallery = () => {
+  return (
+    <div>AnimaterdGallery</div>
+  )
+}
+
+export default AnimaterdGallery
