b6df49df9fb9468246b1e1402ac4fc5aad208385
Eugene Afriyie <groupeight00@gmail.com>
2025-04-07 22:31:57 +0000

    horizontal movement animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import AnimatedCard from './Component/AnimatedCard'
 import ImageGallery from './Component/ImageGallery'
 import AnimaterdGallery from './Component/AnimaterdGallery'
+import Horizontal_movement from './Component/Horizontal_movement'
 
 const App = () => {
   return (
     <div>
-      <AnimatedCard />
+      {/* <AnimatedCard /> */}
       {/* <ImageGallery /> */}
-      <AnimaterdGallery />
+      {/* <AnimaterdGallery /> */}
+      <Horizontal_movement />
     </div>
   )
 }
---
+++ src/Component/Horizontal_movement.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Horizontal_movement = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <motion.div 
+        initial={{ x: 0,y:0, borderRadius: '0%' }}
+        animate={{ x: [-300,0,100],y:[-100,100], borderRadius: ['0%','25%','50%'] }}
+        // exit={{ x: 0, y: 0, borderRadius: '0%' }}
+        transition={{ 
+          duration: 3, 
+          repeat: Infinity, 
+          repeatType: 'reverse' 
+        }}
+        className="h-20 w-20 bg-amber-400"
+      />
+    </div>
+  )
+}
+
+export default Horizontal_movement
