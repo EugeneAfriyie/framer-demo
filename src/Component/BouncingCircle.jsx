abf958b831f7bd0ef28b3b22385f28771d59fff4
Eugene Afriyie <groupeight00@gmail.com>
2025-04-08 23:57:09 +0000

    created bouncing circle

--- src/Component/BouncingCircle.jsx
+++ src/Component/BouncingCircle.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const BouncingCircle = () => {
   return (
-    <div>BouncingCircle</div>
+    <motion.div className='h-32 w-32 bg-amber-600 rounded-full m-auto'
+    initial={{ y: 0 }}
+    animate={{y:100}}
+    transition={{duration:1,repeat:Infinity,repeatType:'reverse'}}
+
+    />
+        
+   
   )
 }
 
