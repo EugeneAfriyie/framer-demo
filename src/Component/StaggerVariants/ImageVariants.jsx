ab6c424f06900e5cc3e34a6e4fcc855a689361c3
Eugene Afriyie <groupeight00@gmail.com>
2025-04-14 23:35:24 +0000

    started staggger trials

---
+++ src/Component/StaggerVariants/ImageVariants.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ImageVariants = () => {
+    const images = [
+        'https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=1470&auto=format&fit=crop',
+        'https://images.unsplash.com/photo-1495567720989-cebdbdd97913?q=80&w=1470&auto=format&fit=crop',
+        'https://images.unsplash.com/photo-1526178615236-1d3b8e60bc04?q=80&w=1470&auto=format&fit=crop',
+        'https://images.unsplash.com/photo-1519183071298-a2962be96b47?q=80&w=1470&auto=format&fit=crop',
+      ];
+    
+  return (
+    <div>ImageVariants</div>
+  )
+}
+
+export default ImageVariants
