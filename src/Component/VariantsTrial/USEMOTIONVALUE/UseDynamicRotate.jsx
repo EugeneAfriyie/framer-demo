0bfa06ac5ca8edba4c9b69e148bcab72c09ca6ab
Eugene Afriyie <groupeight00@gmail.com>
2025-04-16 00:30:38 +0000

    useDynamic trial done

--- src/Component/VariantsTrial/USEMOTIONVALUE/UseDynamicRotate.jsx
+++ src/Component/VariantsTrial/USEMOTIONVALUE/UseDynamicRotate.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { motion, useMotionValue } from 'framer-motion'
 
 const UseDynamicRotate = () => {
+    const rotation = useMotionValue(0);
+    const rotateBox = () => rotation.set(rotation.get() + 90);
   return (
-    <div>useDynamicRotate</div>
+        <motion.div className="  "
+         >
+            Click Me<motion.div className=" w-40 h-40 bg-slate-900 "
+        style={{ rotate: rotation }} onClick={rotateBox } >
+            Click Me
+        </motion.div>
+        </motion.div>
   )
 }
 
